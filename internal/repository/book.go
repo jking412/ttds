7b73694ce3d90049a30be005ff8663d7316a13fe
jking412 <3250532948@qq.com>
2025-05-04 23:32:48 +0800

    feat: add container create handler(if you want use this version,don\'t use container,it exists serious error)

--- internal/repository/book.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-//type CourseReferenceBook struct {
-//	gorm.Model
-//	CourseID  uint   `gorm:"index"`
-//	BookTitle string `gorm:"type:varchar(255)"`
-//	Author    string `gorm:"type:varchar(255)"`
-//}
---
+++ internal/repository/container_template.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"awesomeProject/internal/model"
+	"awesomeProject/pkg/configs"
+	"gorm.io/gorm"
+	"sync"
+)
+
+var (
+	templateRepositoryInstance TemplateRepository
+	templateSyncOnce           sync.Once
+
+	_ TemplateRepository = (*TemplateRepositoryImpl)(nil)
+)
+
+type TemplateRepository interface {
+	GetTemplateByID(id uint) (*model.ContainerTemplate, error)
+}
+
+func NewTemplateRepository(db *gorm.DB) TemplateRepository {
+	templateSyncOnce.Do(func() {
+		if configs.GetConfig().Env == "dev" {
+			templateRepositoryInstance = &DevRepositoryImpl{
+				DB: db,
+			}
+		} else {
+			templateRepositoryInstance = &TemplateRepositoryImpl{
+				DB: db,
+			}
+		}
+	})
+	return templateRepositoryInstance
+}
+
+type TemplateRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func (r *TemplateRepositoryImpl) GetTemplateByID(id uint) (*model.ContainerTemplate, error) {
+	var template model.ContainerTemplate
+	result := r.DB.First(&template, id)
+	return &template, result.Error
+}
+
+type DevRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func (r *DevRepositoryImpl) GetTemplateByID(id uint) (*model.ContainerTemplate, error) {
+	// 不管id，直接返回一个测试用的模板
+	return &model.ContainerTemplate{
+		Name:        "test-container",
+		Description: "测试容器",
+		Image:       "os:test",
+		Ports:       "3001:3000",
+	}, nil
+}
